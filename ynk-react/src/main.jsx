2a182179dab4e7c53b38962044b2cf0ac40ebef3
Yannick Nkongolo <ynkong1@wgu.edu>
2026-04-20 17:50:42 -0500

    Switch to HashRouter to fix direct URL 404s on S3

--- ynk-react/src/main.jsx
+++ ynk-react/src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { LanguageProvider } from './i18n/LanguageContext'
 import App from './App.jsx'
 import './styles/global.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <LanguageProvider>
         <App />
       </LanguageProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
